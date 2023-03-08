60e7c83a24ecad7b977f539469e203888042d796
ZIMAdmin <97988649+ZIMAdmin@users.noreply.github.com>
2023-03-08 13:04:13 -0600

    updating rps code

--- app2.py
+++ app2.py
@@ -1,18 +1,17 @@
 import random
-import pandas as pd
+import time
 from datetime import date
 
 today = date.today()
 playAgainOption = "Yes"
 
 while playAgainOption == "Yes":
-    print("Lets play RPS.  Please choose (R)ock, (P)aper, or (S)cissors \n")
+    print("\nLets play RPS.  Please choose (R)ock, (P)aper, or (S)cissors \n")
     input1 = input()
-
+    time.sleep(3)
     computerChoices = ('Rock', 'Paper', 'Scissor')
     computerAnswer = random.choice(computerChoices)
-
-    if input1 == "R":
+    if input1 == "R" or input1 == "r":
         wordInput = "Rock"
         if computerAnswer == "Rock":
             print ("\n Player chose:" + wordInput +"\nComputer Chose:" + computerAnswer + "\n \n ITS A TIE!")
@@ -20,7 +19,7 @@ while playAgainOption == "Yes":
             print ("\n Player chose:" + wordInput +"\n Computer Chose:" + computerAnswer + "\n \n COMPUTER WINS!")
         if computerAnswer == "Scissors":
             print ("\n Player chose:" + wordInput +"\n Computer Chose:" + computerAnswer + "\n \n YOU WIN!")
-    elif input1 == "S":
+    elif input1 == "S" or input1 == "s":
         wordInput = "Scissors"
         if computerAnswer == "Rock":
             print ("\n Player chose:" + wordInput +"\n Computer Chose:" + computerAnswer + "\n \n COMPUTER WINS!")
@@ -28,7 +27,7 @@ while playAgainOption == "Yes":
             print ("\n Player chose:" + wordInput +"\n Computer Chose:" + computerAnswer + "\n \n YOU WIN!")
         if computerAnswer == "Scissors":
             print ("\n Player chose:" + wordInput +"\n Computer Chose:" + computerAnswer + "\n \n ITS A TIE!")
-    elif input1 == "P":
+    elif input1 == "P" or input1 == "p":
         wordInput = "Paper"
         if computerAnswer == "Rock":
             print ("\n Player chose:" + wordInput +"\n Computer Chose:" + computerAnswer + "\n \n YOU WIN!")
@@ -37,18 +36,19 @@ while playAgainOption == "Yes":
         if computerAnswer == "Scissors":
             print ("\n Player chose:" + wordInput +"\n Computer Chose:" + computerAnswer + "\n \n COMPUTER WINS!")
     else:
-        print("please choose either R, P, or S for (R)ock, (P)aper, or (S)cissors")
-    
+        print("please choose either R, P, or S for (R)ock, (P)aper, or (S)cissors \n")
+        continue
 
-    while playAgainOption2 != "No" and playAgainOption2 != "Yes":
-        print("\n Would you like to play again? (Y)es or (N)o")
-        playAgainOption2 = input()
-        if playAgainOption2 == "Y":
-            playAgainOption2 = "Yes"
-        elif playAgainOption2 == "N":
-            playAgainOption2 = "No"
-        else:
-            print("Please either choose Y or N, for (Y)es or (N)o")
-            
+    time.sleep(3)
+    print("\n Would you like to play again? (Y)es or (N)o")
+    playAgainOption = input()
+    if playAgainOption == "Y" or playAgainOption=="y":
+        playAgainOption = "Yes"
+    elif playAgainOption == "N" or playAgainOption=="n":
+        playAgainOption = "No"
+    else:
+        print("Please either choose Y or N, for (Y)es or (N)o")
+        continue
+    
 
 
