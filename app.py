502ef63fc308947bb03437a92c50576508d69384
ZIMAdmin <97988649+ZIMAdmin@users.noreply.github.com>
2023-03-08 14:46:46 -0600

    updated verbage in app.py code

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import time
 datasource = pd.read_excel("Book1.xlsx")
 loopValue = ""
 
-while loopValue != "KISS THE RING":
+while loopValue != "WE WILL NOT FALL THIS DAY":
     for i, value in datasource.iterrows():
             time.sleep(7)
             print("Execute: \n" + value[0] + "\n")
